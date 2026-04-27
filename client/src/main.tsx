384a5d0aaad0c695e7ee84362f295045b4ab7157
Kaif <22_19074kaif@dsc.du.ac.in>
2026-04-28 00:43:13 +0530

    merge conflict resolve

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import { startBackendKeepAlive } from './utils/keepAlive';
 
 injectSpeedInsights();
+startBackendKeepAlive();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
